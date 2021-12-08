240bea5a4f87c60162810094d1aa4470e51acabc
mwobee <mobetz@gmail.com>
2021-12-08 11:25:08 -0500

    first attempt at fizzbuzz

--- src/VCS.java
+++ src/VCS.java
@@ -86,4 +86,19 @@ public class VCS {
          */
 
     }
+
+
+    public static void FizzBuzz(int counter) {
+        if ( counter % 3 == 0 && counter % 5 == 0 ) {
+            System.out.println("FizzBuzz");
+        } else if ( counter % 3 == 0 ) {
+            System.out.println("Fizz");
+        } else if ( counter % 5 == 0 ) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(counter);
+        }
+    }
+
+
 }
